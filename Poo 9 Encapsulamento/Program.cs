eed2b15c816e76699770f286e813d3590b62b87b
LeeEros <95001466+LeeEros@users.noreply.github.com>
2023-03-22 22:17:52 -0300

    Update Program.cs

--- Poo 9 Encapsulamento/Program.cs
+++ Poo 9 Encapsulamento/Program.cs
@@ -5,6 +5,14 @@ namespace Course
 {
 	class Program
 	{
-		
+		static void Main(string[] args)
+		{
+			Produto p = new Produto();
+			
+			string? name = Console.ReadLine();			
+			
+			p.SetName(name);
+			Console.WriteLine(p.GetName());
+		}
 	}
 }
