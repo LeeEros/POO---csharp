2cc648b351e6a464821de6018056640da908658d
LeeEros <95001466+LeeEros@users.noreply.github.com>
2023-06-21 20:08:40 -0300

    Create Program.cs

---
+++ Poo 12 polimorfismo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Polimorfismo
+{
+	class Animal
+	{
+		public virtual void EmitirSom()
+		{
+			Console.WriteLine("O animal emitiu som")
+;		}
+	}	
+	
+	class Gato : Animal
+	{
+		public override void EmitirSom()
+		{
+			Console.WriteLine("Miau");
+		}
+	}
+	
+	class Cachorro : Animal 
+	{
+		public override void EmitirSom()
+		{
+			Console.WriteLine("Au Au");
+		}
+	}
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Gato xaninho = new Gato();
+			Cachorro titiu = new Cachorro();
+			
+			xaninho.EmitirSom();
+			titiu.EmitirSom();
+		}
+	}
+}
