55ba607e3fac179ea9a900faee9921cb14b5c103
LeeEros <95001466+LeeEros@users.noreply.github.com>
2023-06-21 21:39:27 -0300

    Create Program.cs

---
+++ Poo 12 Polimorfismo interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Polimorfismo_interface
+{
+	interface IAnimal
+	{
+		public void EmitirSom()
+		{
+			Console.WriteLine("O animal emitiu som")
+;		}
+	}	
+	
+	interface Gato : IAnimal
+	{
+		public void EmitirSom()
+		{
+			Console.WriteLine("Miau");
+		}
+	}
+	
+	interface Cachorro : IAnimal 
+	{
+		public void EmitirSom()
+		{
+			Console.WriteLine("Au Au");
+		}
+	}
+	
+	interface Program
+	{
+		static void Main(string[] args)
+		{
+			Gato xaninho = new Gato();
+			Cachorro titiu = new Cachorro();
+			
+			xaninho.EmitirSom();
+			titiu.EmitirSom();
+		}
+	}
+}
